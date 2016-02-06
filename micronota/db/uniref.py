c3ae0b33c20580e128f3931d38f6e9b3b40e8a52
Zech Xu <zhenjiang.xu@gmail.com>
2016-02-05 22:36:22 -0800

    Implement uniref

--- micronota/db/uniref.py
+++ micronota/db/uniref.py
@@ -13,13 +13,39 @@ UniRef
 # The full license is in the file COPYING.txt, distributed with this software.
 # ----------------------------------------------------------------------------
 
-import os
+from os.path import join, expanduser
+from sqlite3 import connect
+
+from skbio import read
 
 
 def prepare_db(out_d, prefix='uniref', force=False):
     '''
     '''
+    prepare_metadata(join(expanduser('~'), 'uniref'), prefix)
 
-def prepare_metadata():
+
+def prepare_metadata(in_fp, fp):
     '''
     '''
+    with connect(fp) as conn:
+        conn.execute("DROP TABLE IF EXISTS uniref")
+        conn.execute('''CREATE TABLE uniref (
+                            id       TEXT    NOT NULL,
+                            tag      TEXT    NOT NULL,
+                            value    BLOB    NOT NULL,
+                            transfer BOOLEAN NOT NULL,
+                        CHECK (transfer IN (0,1)));''')
+
+        for seq in read(in_fp, format='embl'):
+            md = seq.metadata
+            id = md['AC']
+            insert = '''INSERT INTO uniref (id, tag, value, transfer)
+                        VALUES (?,?,?,?);'''
+            conn.execute(insert % (id, 'OX', md['OX'], 0))
+            conn.execute(insert % (id, 'PE', md['PE'], 0))
+            kingdom = md['OC'].split('; ', 1)[0]
+            conn.execute(insert % (id, 'KD', kingdom, 0))
+        # don't forget to index the column to speed up query
+        conn.execute('CREATE INDEX id ON uniref (id);')
+        conn.commit()
