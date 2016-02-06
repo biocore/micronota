15250319e89a4d94827d7825e1b1fab5541ace1d
Zech Xu <zhenjiang.xu@gmail.com>
2016-02-05 18:34:09 -0800

    Add uniref into db module

---
+++ micronota/db/uniref.py
@@ -0,0 +1,25 @@
+r'''
+UniRef
+======
+
+'''
+
+
+# ----------------------------------------------------------------------------
+# Copyright (c) 2015--, micronota development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import os
+
+
+def prepare_db(out_d, prefix='uniref', force=False):
+    '''
+    '''
+
+def prepare_metadata():
+    '''
+    '''
