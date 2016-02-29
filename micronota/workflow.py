587a76d18176dbe8b62b52cbe281ec0b1420b776
Zech Xu <zhenjiang.xu@gmail.com>
2016-02-29 11:24:25 -0800

    change recursive func to for loop
    and other misc changes

--- micronota/workflow.py
+++ micronota/workflow.py
@@ -6,7 +6,7 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 # ----------------------------------------------------------------------------
 
-from os.path import splitext, basename, join
+from os.path import splitext, basename, join, exists
 from os import remove
 from importlib import import_module
 from tempfile import NamedTemporaryFile
@@ -34,7 +34,7 @@ def annotate(in_fp, in_fmt, out_dir, out_fmt, kingdom, cpus, config):
             if sec == 'FEATURE':
                 im.update(identify_all_features(seq, seq_tmp_dir, config[sec]))
             if sec == 'CDS':
-                annotate_all_cds(im, out_dir, kingdom, config[sec])
+                annotate_all_cds(seq, out_dir, kingdom, config[sec])
 
         seq.interval_metadata.concat(im, inplace=True)
         seq.write(out, format=out_fmt)
@@ -81,31 +81,33 @@ def identify_all_features(seq, out_dir, tasks,
     return im
 
 
-def annotate_all_cds(im, out_dir, kingdom, tasks,
+def annotate_all_cds(seq, out_dir, kingdom, tasks,
                      search_func='search_protein_homologs',
                      parse_func='parse_output'):
     '''Annotate CDS.'''
-    uniref_dbs = ['uniref100_Swiss-Prot_Bacteria.fasta',
-                  'uniref100_Swiss-Prot_Archaea.fasta',
-                  'uniref100_Swiss-Prot_Viruses.fasta',
-                  'uniref100_Swiss-Prot_Eukaryota.fasta',
-                  'uniref100_Swiss-Prot_other.fasta',
-                  'uniref100_TrEMBL_Bacteria.fasta',
-                  'uniref100_TrEMBL_Archaea.fasta',
-                  'uniref100_TrEMBL_Viruses.fasta',
-                  'uniref100_TrEMBL_Eukaryota.fasta',
-                  'uniref100_TrEMBL_other.fasta',
-                  'uniref100__other.fasta']
+    uniref_dbs = ['uniref100_Swiss-Prot_Bacteria',
+                  'uniref100_Swiss-Prot_Archaea',
+                  'uniref100_Swiss-Prot_Viruses',
+                  'uniref100_Swiss-Prot_Eukaryota',
+                  'uniref100_Swiss-Prot_other',
+                  'uniref100_TrEMBL_Bacteria',
+                  'uniref100_TrEMBL_Archaea',
+                  'uniref100_TrEMBL_Viruses',
+                  'uniref100_TrEMBL_Eukaryota',
+                  'uniref100_TrEMBL_other',
+                  'uniref100__other']
 
     uniref_dbs = [join(_DB_PATH, i) for i in uniref_dbs]
     order = {'bacteria': range(11),
              'archaea': [1, 0, 2, 3, 4, 6, 5, 7, 8, 9, 10],
              'viruses': [2, 0, 1, 3, 4, 7, 5, 6, 8, 9, 10]}
 
-    id_old_features = {}
+    im = seq.interval_metadata
+    id_old_cds = dict()
+    id_new_cds = dict()
     for feature in im:
         if feature['type_'] == 'CDS':
-            id_old_features[feature['id']] = feature
+            id_old_cds[feature['id']] = feature
 
     for tool in tasks:
         value = tasks[tool]
@@ -116,29 +118,28 @@ def annotate_all_cds(im, out_dir, kingdom, tasks,
 
         if value == 'uniref':
             dbs = [uniref_dbs[i] for i in order[kingdom.lower()]]
+            for db in dbs:
+                db = join(_DB_PATH, db)
+                if not exists('%.dmnd' % db):
+                    continue
 
-            def _uniref(id_old_features, dbs, id_new_features=None):
-                if id_new_features is None:
-                    id_new_features = dict()
-                if not id_old_features or not dbs:
-                    return id_new_features
+                tmp = NamedTemporaryFile('w+', delete=False)
+                out = open(tmp.name, 'w')
+                for i in id_old_cds:
+                    pro = Sequence(id_old_cds[i]['translation'], {'id': i})
+                    pro.write(out, format='fasta')
 
-                for i in id_old_features:
-                    tmp = NamedTemporaryFile('w+', delete=False)
-                    pro = Sequence(id_old_features[i]['translation'], {'id': i})
-                    pro.write(tmp.name, format='fasta')
-                db = dbs[0]
                 res = search_f(tmp, db, out_dir)
                 hits = parse_f(res)
                 for idx, row in hits.iterrows():
-                    feature = id_old_features.pop(idx)
-                    id_new_features[idx] = feature.update(db_xref=row['sseqid'])
+                    old_cds = id_old_cds.pop(idx)
+                    new_cds = feature.update(db_xref=row['sseqid'])
+                    im.update(old_cds, new_cds)
 
-                    tmp.close()
-                    remove(tmp.name)
-                return _uniref(id_old_features, dbs[1:], id_new_features)
-            id_new_features = _uniref(id_old_features, dbs)
-            for id in id_new_features:
-                im[id_new_features[id]] = im.pop(id_old_features[id])
+                out.close()
+                remove(tmp.name)
+
+            for id in id_new_cds:
+                im[id_new_cds[id]] = im.pop(id_old_cds[id])
 
         return im
