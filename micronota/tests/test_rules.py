55c998007146e8ff62b3a26127ef16360a25962d
Zech Xu <zhenjiang.xu@gmail.com>
2016-11-01 01:46:08 -0700

    update test_rules

--- micronota/tests/test_rules.py
+++ micronota/tests/test_rules.py
@@ -7,111 +7,124 @@
 # ----------------------------------------------------------------------------
 
 from unittest import TestCase, skipIf, main
-from shutil import which, rmtree
-from os.path import join, abspath, dirname
+from pkg_resources import resource_filename
+from shutil import which
+from os.path import join
 from os import mkdir
-from tempfile import mkdtemp
-from subprocess import Popen, PIPE
+from tempfile import TemporaryDirectory, mkdtemp
+
+from snakemake.logging import logger, setup_logger
+from snakemake import snakemake
 
 
 class TestRules(TestCase):
     def setUp(self):
-        self.tmpd = mkdtemp()
-        self.snakefile = join(self.tmpd, 'Snakefile')
-        self.path = abspath(dirname(__file__))
         self.seq_fn = 'test.fna'
-        self.out_path = join(self.tmpd, self.seq_fn)
-        self.template = '''workdir: '{wd}'
-fn = 'test.fna'
-tool_output = '{o}'
-tool_config = %r
-config['seq'] = '{i}'
-config['genetic_code'] = 2
-include: '%s'
-'''.format(wd=self.tmpd, i=self.input_seq, o=self.out_path)
+        self.snakefile = resource_filename('micronota', 'rules/Snakefile')
+        self.config = {'seq': 'test.fna', 'genetic_code': 11}
+        self.tmpd = mkdtemp()
 
-    def _run_snakemake(self, tool, s):
-        with open(self.snakefile, 'w') as out:
-            out.write(self.template % (
-                s, join(dirname(self.path), '%s.rule' % tool)))
-        p = Popen(['snakemake', '-s', self.snakefile, '-F', '-p', '-n',
-                   '--cores', '8',
-                   '--keep-target-files', self.out_path + '.gff',
-                   '--config', 'output_dir=%s' % self.tmpd],
-                  universal_newlines=True, stdout=PIPE, )
-        stdout, stderr = p.communicate()
-        return stdout
+    def _run_snakemake(self, config):
+        open(join(self.tmpd, self.seq_fn), 'w').close()
+        success = snakemake(
+            self.snakefile,
+            cores=8,
+            workdir=self.tmpd,
+            dryrun=True,
+            forcetargets=True,
+            config=self.config,
+            keep_target_files=True,
+            keep_logger=True)
+        with open(logger.logfile) as o:
+            return o.read()
 
     @skipIf(which("aragorn") is None, 'Aragorn not installed.')
     def test_aragorn(self):
-        stdout = self._run_snakemake(
-            'aragorn', {'params': '-l', 'priority': 50, 'threads': 1})
-        exp = 'aragorn -l -gc2 -o {o}.txt {i} &> {o}.log'.format(
-            o=self.out_path, i=self.input_seq)
-        self.assertIn(exp, stdout)
-        self.assertIn('priority: 50', stdout)
+        self.config['tools'] = {'aragorn':
+                                {'params': '-l',
+                                 'priority': 50,
+                                 'threads': 1}}
+        log = self._run_snakemake(self.config)
+        exp = ('aragorn -l -gc11 -o aragorn/{0}.txt {0} &> aragorn/{0}.log && '
+               'touch aragorn/{0}.ok').format(self.seq_fn)
+        self.assertIn(exp, log)
 
     @skipIf(which("minced") is None, 'minced not installed.')
     def test_minced(self):
-        stdout = self._run_snakemake(
-            'minced', {'params': '', 'priority': 50, 'threads': 1})
-        exp = 'minced  -gff {i} {o}.gff &> {o}.log'.format(
-            o=self.out_path, i=self.input_seq)
-        self.assertIn(exp, stdout)
-        self.assertIn('priority: 50', stdout)
+        self.config['tools'] = {'minced':
+                                {'params': '',
+                                 'priority': 50,
+                                 'threads': 1}}
+        log = self._run_snakemake(self.config)
+        exp = ('minced  -gff {0} minced/{0}.gff &> minced/{0}.log && '
+               'touch minced/{0}.ok').format(self.seq_fn)
+        self.assertIn(exp, log)
 
     @skipIf(which("prodigal") is None, 'Prodigal not installed.')
     def test_prodigal(self):
-        stdout = self._run_snakemake(
-            'prodigal', {'params': '-p meta', 'priority': 90, 'threads': 1})
-        exp = ('prodigal -p meta -f gff -g 2 -i {i} -o {o}.gff'
-               ' -a {o}.faa -d {o}.fna &> {o}.log'.format(
-                   o=self.out_path, i=self.input_seq))
-        self.assertIn(exp, stdout)
-        self.assertIn('priority: 90', stdout)
+        self.config['tools'] = {'prodigal':
+                                {'params': '-p meta',
+                                 'priority': 90,
+                                 'threads': 1}}
+        log = self._run_snakemake(self.config)
+        exp = ('prodigal -p meta -f gff -g 11 -i {0} -o prodigal/{0}.gff'
+               ' -a prodigal/{0}.faa -d prodigal/{0}.fna &>'
+               ' prodigal/{0}.log && '
+               'touch prodigal/{0}.ok').format(self.seq_fn)
+        self.assertIn(exp, log)
 
     @skipIf(which("cmscan") is None, 'Infernal not installed.')
     def test_cmscan(self):
-        db = join(self.tmpd, 'rfam.cm')
-        open(db, 'w').close()
-        config = {'params': '', 'priority': 30, 'threads': 2, 'db': db}
-        stdout = self._run_snakemake('cmscan', config)
-        exp = 'cmscan  --cpu {n} --tblout {o}.txt {db} {i} &> {o}.log'.format(
-            o=self.out_path, i=self.input_seq, db=config['db'], n=config['threads'])
-        self.assertIn(exp, stdout)
-        self.assertIn('priority: 30', stdout)
+        with TemporaryDirectory() as tmpd:
+            db = join(tmpd, 'rfam.cm')
+            open(db, 'w').close()
+            self.config['tools'] = {'cmscan':
+                                    {'params': '',
+                                     'priority': 30,
+                                     'threads': 2,
+                                     'db': db}}
+            log = self._run_snakemake(self.config)
+        exp = ('cmscan  --cpu {n} --tblout cmscan/{o}.txt {db} {o} &>'
+               ' cmscan/{o}.log && touch cmscan/{o}.ok').format(
+                   o=self.seq_fn, db=db, n=2)
+        self.assertIn(exp, log)
 
     @skipIf(which("diamond") is None, 'diamond not installed.')
     def test_diamond(self):
-        db = self.tmpd
-        open(join(db, 'uniref90.dmnd'), 'w').close()
-        open(join(db, 'uniref50.dmnd'), 'w').close()
-        mkdir(join(self.tmpd, 'prodigal'))
-        i = join(self.tmpd, 'prodigal', self.seq_fn + '.faa')
-        open(i, 'w').close()
-        config = {'params': '--index-chunks 1 --query-cover 80 -k 3',
-                  'priority': 80, 'threads': 4, 'db': db}
-        stdout = self._run_snakemake('diamond', config)
+        with TemporaryDirectory() as tmpd:
+            db = tmpd
+            open(join(db, 'uniref90.dmnd'), 'w').close()
+            open(join(db, 'uniref50.dmnd'), 'w').close()
+            d = join(self.tmpd, 'prodigal')
+            mkdir(d)
+            open(join(d, self.seq_fn + '.faa'), 'w').close()
+            config = {'params': '--index-chunks 1 --query-cover 80 -k 3',
+                      'priority': 80, 'threads': 4, 'db': db}
+            self.config['tools'] = {'diamond': config}
+            log = self._run_snakemake(self.config)
+
         exp = ('diamond blastp {p} --threads {n} --db {db}/uniref90.dmnd -q {i}'
                ' -a {o}_uniref90.daa &> {o}_uniref90.daa.log'.format(
                    p=config['params'],
                    n=config['threads'],
                    db=config['db'],
-                   o=self.out_path, i=i))
-        self.assertIn(exp, stdout)
-        unmatched = join(self.tmpd, self.seq_fn + '_uniref90_unmatched.faa')
+                   o='diamond/%s' % self.seq_fn,
+                   i='prodigal/%s.faa' % self.seq_fn))
+        self.assertIn(exp, log)
+
+        unmatched = join('diamond', self.seq_fn + '_uniref90_unmatched.faa')
         exp = ('diamond blastp {p} --threads {n} --db {db}/uniref50.dmnd -q {i}'
                ' -a {o}_uniref50.daa &> {o}_uniref50.daa.log'.format(
                    p=config['params'],
                    n=config['threads'],
                    db=config['db'],
-                   o=self.out_path, i=unmatched))
-        self.assertIn(exp, stdout)
-        self.assertIn('priority: 80', stdout)
-
-    def tearDown(self):
-        rmtree(self.tmpd)
+                   o='diamond/%s' % self.seq_fn, i=unmatched))
+        self.assertIn(exp, log)
 
 
 if __name__ == '__main__':
+    # this is required to set snakemake logging to files correctly
+    setup_logger(printshellcmds=True)
+    logger.logger.removeHandler(logger.logger.handlers[1])
+
     main()
